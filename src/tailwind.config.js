3d6d62d396fc50288a00de68c2d8d3b443ede450
luluh04 <luluhfasilia5@gmail.com>
2025-10-24 21:55:33 +0700

    fist commit

---
+++ src/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // cek semua file html
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'], // nambahin font poppins
+      },
+    },
+  },
+  plugins: [],
+}
